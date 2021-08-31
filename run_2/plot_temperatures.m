b78efeea3483d570ba18f2a86fdb1240d627a218
Yuka Nakato <nakat013@umn.edu>
2021-08-31 15:21:26 -0700

    Making 4K only and 50K only plots, and log plots as well

--- run_2/plot_temperatures.m
+++ run_2/plot_temperatures.m
@@ -31,4 +31,110 @@ title('BA4 Run 2 Temperatures');
 datetick('x', 'mm/dd', 'keeplimits');
 
 % Save
-print('ba4_run2_temp', '-dpng');
+print('ba4p2_cooldown_all_lin', '-dpng');
+
+% Log plot now
+figure(2);
+clf;
+setwinsize(gcf,800,600);
+semilogy(time, f.antenna0.hk0.slow_temp(:,28), 'k-');
+hold on;
+semilogy(time, f.antenna0.hk0.slow_temp(:,29), 'r-');
+semilogy(time, f.antenna0.hk0.slow_temp(:,30), 'y-');
+semilogy(time, f.antenna0.hk0.slow_temp(:,31), 'g-');
+semilogy(time, f.antenna0.hk0.slow_temp(:,32), 'c-');
+semilogy(time, f.antenna0.hk0.slow_temp(:,33), 'b-');
+semilogy(time, f.antenna0.hk0.slow_temp(:,34), 'm-');
+semilogy(time, f.antenna0.hk0.slow_temp(:,35), 'Color', [0.4940 0.1840 0.5560]);
+legend('50K cold head','4K heat strap cold side','4K heat strap warm side','50K heat strap cold side','50K heat strap warm side','4K baseplate','4K tube top','50K tube top');
+xlabel('Date');
+ylabel('Temperature [K]');
+title('BA4 Run 2 Temperatures');
+
+% Change x display to user friendly UTC
+datetick('x', 'mm/dd', 'keeplimits');
+
+% Save
+print('ba4p2_cooldown_all_log', '-dpng');
+
+% Repeat for 4K only
+% Linear plot
+figure(3);
+clf;
+setwinsize(gcf,800,600);
+plot(time, f.antenna0.hk0.slow_temp(:,29), 'r-');
+hold on;
+plot(time, f.antenna0.hk0.slow_temp(:,30), 'y-');
+plot(time, f.antenna0.hk0.slow_temp(:,33), 'b-');
+plot(time, f.antenna0.hk0.slow_temp(:,34), 'm-');
+legend('4K heat strap cold side','4K heat strap warm side','4K baseplate','4K tube top');
+xlabel('Date');
+ylabel('Temperature [K]');
+title('BA4 Run 2 4K Temperatures');
+
+% Change x display to user friendly UTC
+datetick('x', 'mm/dd', 'keeplimits');
+
+% Save
+print('ba4p2_cooldown_4k_lin', '-dpng');
+
+% Log plot now
+figure(4);
+clf;
+setwinsize(gcf,800,600);
+semilogy(time, f.antenna0.hk0.slow_temp(:,29), 'r-');
+hold on;
+semilogy(time, f.antenna0.hk0.slow_temp(:,30), 'y-');
+semilogy(time, f.antenna0.hk0.slow_temp(:,33), 'b-');
+semilogy(time, f.antenna0.hk0.slow_temp(:,34), 'm-');
+legend('4K heat strap cold side','4K heat strap warm side','4K baseplate','4K tube top');
+xlabel('Date');
+ylabel('Temperature [K]');
+title('BA4 Run 2 4K Temperatures');
+
+% Change x display to user friendly UTC
+datetick('x', 'mm/dd', 'keeplimits');
+
+% Save
+print('ba4p2_cooldown_4k_log', '-dpng');
+
+% Repeat for 50K only
+% Linear plot
+figure(5);
+clf;
+setwinsize(gcf,800,600);
+plot(time, f.antenna0.hk0.slow_temp(:,28), 'k-');
+hold on;
+plot(time, f.antenna0.hk0.slow_temp(:,31), 'g-');
+plot(time, f.antenna0.hk0.slow_temp(:,32), 'c-');
+plot(time, f.antenna0.hk0.slow_temp(:,35), 'Color', [0.4940 0.1840 0.5560]);
+legend('50K cold head','50K heat strap cold side','50K heat strap warm side','50K tube top');
+xlabel('Date');
+ylabel('Temperature [K]');
+title('BA4 Run 2 50K Temperatures');
+
+% Change x display to user friendly UTC
+datetick('x', 'mm/dd', 'keeplimits');
+
+% Save
+print('ba4p2_cooldown_50k_lin', '-dpng');
+
+% Log plot now
+figure(6);
+clf;
+setwinsize(gcf,800,600);
+semilogy(time, f.antenna0.hk0.slow_temp(:,28), 'k-');
+hold on;
+semilogy(time, f.antenna0.hk0.slow_temp(:,31), 'g-');
+semilogy(time, f.antenna0.hk0.slow_temp(:,32), 'c-');
+semilogy(time, f.antenna0.hk0.slow_temp(:,35), 'Color', [0.4940 0.1840 0.5560]);
+legend('50K cold head','50K heat strap cold side','50K heat strap warm side','50K tube top');
+xlabel('Date');
+ylabel('Temperature [K]');
+title('BA4 Run 2 50K Temperatures');
+
+% Change x display to user friendly UTC
+datetick('x', 'mm/dd', 'keeplimits');
+
+% Save
+print('ba4p2_cooldown_50k_log', '-dpng');
