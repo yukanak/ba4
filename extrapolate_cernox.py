359a81df2d9afb5d47fd191be1875a7581ce79b3
Yuka Nakato <nakat013@umn.edu>
2022-11-30 00:35:44 -0500

    Adding to Cernox extrapolation stuff

--- extrapolate_cernox.py
+++ extrapolate_cernox.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-def extrapolate_cernox(loc, res_val, hk_val=None):
+def extrapolate_cernox(loc, res_val):
     '''
     If the measured Cernox resistance is out of the calibration range, we want
     to predict the temperature by doing a rough exponential extrapolation
@@ -10,6 +10,38 @@ def extrapolate_cernox(loc, res_val, hk_val=None):
     The loc should be either 'uc evap', 'ic evap', 'he4 evap', or 'he4 cond'.
     The argument res_val should be the resistance in Ohms.
     '''
+    temp, res, title, x_interp, extrap_max = get_cernox_data(loc)
+    # Fit an exponential function, which would look like a line on log-log plot
+    if res_val > res[0]:
+        popt, pcov = curve_fit(ExpFunc, temp[:extrap_max], res[:extrap_max])
+        # Get temperature value for the measured resistance
+        temp_val = (res_val/popt[0])**(1/popt[1])
+    elif res_val > res[-1]:
+        idx = find_nearest(res, res_val)
+        popt, pcov = curve_fit(ExpFunc, temp[idx-20:idx+20], res[idx-20:idx+20])
+        temp_val = (res_val/popt[0])**(1/popt[1])
+        x_interp = np.linspace(temp[idx-20], temp[idx+20], 100)
+    else:
+        print("Resistance is too small; room temperature!")
+        temp_val = np.nan
+    return temp_val
+
+def plot_extrapolation(loc, res_val, hk_val=None):
+    temp_val = extrapolate_cernox(loc, res_val)
+    temp, res, title, x_interp, extrap_max = get_cernox_data(loc)
+    plt.loglog(temp, res,'b.', label='Duband Calibration')
+    plt.loglog(x_interp, ExpFunc(x_interp, *popt), color='g', linestyle='--', label='Fit Line')
+    plt.loglog(temp_val, res_val, 'rx', label=f'{temp_val:.3f} K at {res_val} Ohms')
+    if hk_val:
+        plt.loglog(hk_val, res_val, color='y', marker='x', linestyle='', label=f'HK Temperature Value: {hk_val:.2f} K')
+    plt.xlabel('Temperature [K]')
+    plt.ylabel('Resistance [Ohm]')
+    plt.title(title)
+    plt.legend()
+    plt.show()
+    return temp_val
+
+def get_cernox_data(loc):
     d = np.genfromtxt(open("ba4_fridge_cernox_data.csv", "rb"), delimiter=",", filling_values=0, skip_header=1)
     if loc == 'uc evap':
         temp = d[:12503,6]
@@ -36,35 +68,34 @@ def extrapolate_cernox(loc, res_val, hk_val=None):
         title = 'He4 Condensation Point'
         extrap_max = 8000
         x_interp = np.linspace(3, temp[extrap_max], 1000)
+    elif loc == 'extrapolated uc evap':
+        d = np.genfromtxt(open("thermometers/R2T_X120258.interp", "rb"), delimiter=" ", filling_values=0)
+        temp = d[:,1]
+        res = d[:,0]
+        title = 'UC Evaporator (Extrapolated)'
+        x_interp = np.linspace(0.2, 0.315, 100)
+        extrap_max = 4000
+        x_interp = np.linspace(0.2, temp[extrap_max], 100)
     else:
-        raise ValueError("Argument loc must be 'uc evap', 'ic evap', 'he4 evap', or 'he4 cond'")
+        raise ValueError("Argument loc must be 'uc evap', 'ic evap', 'he4 evap', 'he4 cond', 'extrapolated uc evap', 'S1', or 'S3'")
+    return temp, res, title, x_interp, extrap_max
 
-    # Fit an exponential function, which would look like a line on log-log plot
-    if res_val > res[0]:
-        popt, pcov = curve_fit(ExpFunc, temp[:extrap_max], res[:extrap_max])
-        # Get temperature value for the measured resistance
-        temp_val = (res_val/popt[0])**(1/popt[1])
-    elif res_val > res[-1]:
-        idx = find_nearest(res, res_val)
-        popt, pcov = curve_fit(ExpFunc, temp[idx-20:idx+20], res[idx-20:idx+20])
-        temp_val = (res_val/popt[0])**(1/popt[1])
-        x_interp = np.linspace(temp[idx-20], temp[idx+20], 100)
-    else:
-        print("Resistance is too small; room temperature!")
-        temp_val = np.nan
-
+def plot_uc_duband_vs_extrapolated():
+    temp, res, title, x_interp, extrap_max = get_cernox_data('uc evap')
+    t, r, tit, x_int, ex_max = get_cernox_data('extrapolated uc evap')
     plt.loglog(temp, res,'b.', label='Duband Calibration')
-    plt.loglog(x_interp, ExpFunc(x_interp, *popt), color='g', linestyle='--', label='Fit Line')
-    plt.loglog(temp_val, res_val, 'rx', label=f'{temp_val:.3f} K at {res_val} Ohms')
-    if hk_val:
-        plt.loglog(hk_val, res_val, color='y', marker='x', linestyle='', label=f'HK Temperature Value: {hk_val:.2f} K')
+    plt.loglog(t, r,'r.', label='Extrapolated Calibration in GCP')
     plt.xlabel('Temperature [K]')
     plt.ylabel('Resistance [Ohm]')
-    plt.title(title)
+    plt.title('Duband vs GCP UC Calibration Curve')
     plt.legend()
     plt.show()
 
-    return temp_val
+
+
+
+def plot_bias_power(bias_voltages, resistance_vals, locations=['ic evap','uc evap','S1','S3']):
+    pass
 
 def ExpFunc(x, a, b):
     return a * np.power(x, b)
